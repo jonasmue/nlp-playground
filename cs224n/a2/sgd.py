7d13e7c202cc03990d279c7eed5bff05c48c7500
Jonas Müller <jonas.mueller@compitencies.com>
2020-05-31 11:43:58 +0200

    Finish assignment 2

--- cs224n/a2/sgd.py
+++ cs224n/a2/sgd.py
@@ -84,9 +84,9 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
     for iter in range(start_iter + 1, iterations + 1):
         # You might want to print the progress every few iterations.
 
-        loss = None
         ### YOUR CODE HERE (~2 lines)
-
+        loss, gradient = f(x)
+        x -= step * gradient
         ### END YOUR CODE
 
         x = postprocessing(x)
